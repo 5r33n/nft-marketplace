e48ad8e937b6e4d56beb4a71f3e45b92333e13f1
5r33n <5r33ns@gmail.com>
2022-12-19 07:55:06 +0000

    cleaned

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,27 +1,27 @@
-const { ethers } = require("hardhat");
-const hre = require("hardhat");
-const fs = require("fs");
+const { ethers } = require("hardhat")
+const hre = require("hardhat")
+const fs = require("fs")
 
 async function main() {
-  const [deployer] = await ethers.getSigners();
-  const balance = await deployer.getBalance();
-  const Marketplace = await hre.ethers.getContractFactory("NFTMarketplace");
-  const marketplace = await Marketplace.deploy();
+  const [deployer] = await ethers.getSigners()
+  const balance = await deployer.getBalance()
+  const Marketplace = await hre.ethers.getContractFactory("NFTMarketplace")
+  const marketplace = await Marketplace.deploy()
 
-  await marketplace.deployed();
+  await marketplace.deployed()
 
   const data = {
     address: marketplace.address,
-    abi: JSON.parse(marketplace.interface.format('json'))
+    abi: JSON.parse(marketplace.interface.format("json")),
   }
 
   //This writes the ABI and address to the mktplace.json
-  fs.writeFileSync('./src/Marketplace.json', JSON.stringify(data))
+  fs.writeFileSync("./src/Marketplace.json", JSON.stringify(data))
 }
 
 main()
   .then(() => process.exit(0))
   .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    console.error(error)
+    process.exit(1)
+  })
